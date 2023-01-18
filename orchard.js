4b59b73b0afb4a9e2ed15b94f99e17224f1d998c
Keldon Lopez <keldon.lopz@gmail.com>
2023-01-17 21:28:10 -0700

    Completed problem 5 of orchard.js

--- orchard.js
+++ orchard.js
@@ -57,26 +57,26 @@ let pinkTotals = 0;
 //I will first get the total for fujiAcres
 for (let index = 0; index < fujiAcres.length; index++) {
   fujiTotals += fujiAcres[index];
-//   console.log(fujiTotals);
+  //   console.log(fujiTotals);
 }
 
 // I am now getting the total for picked for galaAcres
 for (let index = 0; index < galaAcres.length; index++) {
-    galaTotals += galaAcres[index];
-    // console.log(galaTotals);
-  }
+  galaTotals += galaAcres[index];
+  // console.log(galaTotals);
+}
 
 //   lastly getting the total for picked for pinkAcres
 for (let index = 0; index < pinkAcres.length; index++) {
-    pinkTotals += pinkAcres[index];
-    // console.log(pinkTotals);
-  }
+  pinkTotals += pinkAcres[index];
+  // console.log(pinkTotals);
+}
 
-  // Now I add them all to totalAcres and log totalAcres
+// Now I add them all to totalAcres and log totalAcres
 totalAcres += fujiTotals;
 totalAcres += galaTotals;
 totalAcres += pinkTotals;
-console.log("Total number of acres picked",totalAcres);
+console.log("Total number of acres picked", totalAcres, "acres.");
 
 // PROBLEM 2
 
@@ -91,6 +91,14 @@ console.log("Total number of acres picked",totalAcres);
 */
 
 // CODE HERE
+// I just need to take the total acres and divide it by the total number of days which is 7 days
+
+let averageDailyAcres = totalAcres / 7;
+console.log(
+  "Average daily acres picked over 7 days is",
+  averageDailyAcres,
+  "acres."
+);
 
 // PROBLEM 3
 
@@ -125,6 +133,17 @@ let acresLeft = 174;
 let days = 0;
 
 // CODE HERE
+// I created a while loop that first adds 1 to days and then subtracts 7 from acresLeft;
+
+while (acresLeft > 0) {
+  days += 1;
+  acresLeft -= 7;
+}
+console.log(
+  "It should take",
+  days,
+  "days on average to finish the remaining acres."
+);
 
 // PROBLEM 4
 
@@ -152,9 +171,34 @@ let days = 0;
 
 // CODE HERE
 
-// let fujiTons =
-// let galaTons =
-// let pinkTons =
+// I am first gonna comment the acres on the above lines so I don't need to scroll
+// const fujiAcres = [2, 3, 3, 2, 2, 2, 1];
+// const galaAcres = [5, 2, 4, 3, 6, 2, 4];
+// const pinkAcres = [1, 5, 4, 2, 1, 5, 4];
+// I uncommented the provided empty arrays and used a for loop to push the amount of acres picked times 6.5 tons to get the amount of tons picked per day.
+// I also put console logs so I could test that each loop was working as intended.
+
+let fujiTons = [];
+let galaTons = [];
+let pinkTons = [];
+
+for (let index = 0; index < fujiAcres.length; index++) {
+  fujiTons.push(fujiAcres[index] * 6.5);
+  //   console.log(fujiTons);
+}
+console.log("Daily total in tons of Fuji Apples picked ", fujiTons);
+
+for (let index = 0; index < fujiAcres.length; index++) {
+  galaTons.push(galaAcres[index] * 6.5);
+  //   console.log(galaTons);
+}
+console.log("Daily total in tons of Gala Apples picked ", galaTons);
+
+for (let index = 0; index < fujiAcres.length; index++) {
+  pinkTons.push(pinkAcres[index] * 6.5);
+  //   console.log(pinkTons);
+}
+console.log("Daily total in tons of Pink Apples picked ", pinkTons);
 
 // PROBLEM 5
 
@@ -174,9 +218,36 @@ let days = 0;
 
 // CODE HERE
 
-// let fujiPounds =
-// let galaPounds =
-// let pinkPounds =
+let fujiTotalTons = 0;
+let galaTotalTons = 0;
+let pinkTotalTons = 0;
+
+// let fujiPounds = 0;
+// let galaPounds = 0;
+// let pinkPounds = 0;
+
+//I will first get the total tons the same way I got the total acres then take the total tons times 2000 to get the total pounds for each for loop
+
+for (let index = 0; index < fujiTons.length; index++) {
+  fujiTotalTons += fujiTons[index];
+  // console.log("Total Fuji tons",fujiTotalTons);
+}
+let fujiPounds = fujiTotalTons * 2000;
+console.log("There are", fujiPounds, "pounds of Fuji Apples.");
+
+for (let index = 0; index < galaTons.length; index++) {
+  galaTotalTons += galaTons[index];
+  //   console.log("Total Gala tons",galaTotalTons);
+}
+let galaPounds = galaTotalTons * 2000;
+console.log("There are", galaPounds, "pounds of Gala Apples.");
+
+for (let index = 0; index < pinkTons.length; index++) {
+  pinkTotalTons += pinkTons[index];
+  //   console.log("Total Pink tons",pinkTotalTons);
+}
+let pinkPounds = pinkTotalTons * 2000;
+console.log("There are", pinkPounds, "pounds of Pink Apples.");
 
 // PROBLEM 6
 
